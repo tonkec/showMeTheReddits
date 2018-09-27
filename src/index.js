2683672fa79c21a175cf648c82db87a7c3256766
Antonija Simic <antonija1023@gmail.com>
2018-09-27 12:25:30 +0200

    filter data by category based on the value from input

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
@@ -13,23 +12,38 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 class Jobs extends React.Component {
   constructor(props) {
     super(props);
+    this.getKeyword = this.getKeyword.bind(this);
+    this.getPosts = this.getPosts.bind(this);
     this.state = {
       posts: [],
       loading: true,
-      error: null
+      error: null,
+      category:  "python" // default value
     };
   }
 
-  componentDidMount() {
-    axios.get('https://careers.codeinstitute.net/job-listings/1?keyword=python&location=ireland&type=full-time')
+  componentDidMount(){
+    this.getPosts(this.state.category)
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (this.state.category !== nextState.category) {
+      this.getPosts(nextState.category)
+    }
+  }
+
+  getPosts(category){
+     console.log(`fetching... ${this.state.category}`)
+     axios.get(`https://careers.codeinstitute.net/job-listings/1?category=${category}&location=ireland&type=full-time`)
       .then(res => {
-      
         const posts = res.data.data.map(obj => JSON.parse(obj))
         this.setState({
-          posts,
+          posts: posts,
           loading: false,
           error: null
         });
+        console.log(`fetched: ${res.data.data}`)
+
       })
       .catch(err => {
         this.setState({
@@ -56,16 +70,39 @@ class Jobs extends React.Component {
       return this.renderError();
     }
   }
+  
+  getKeyword(e, keyword){
+    e.preventDefault();
+    keyword = e.target.elements.keyword.value.trim();
+    this.setState(() => ({
+      category: keyword
+    }));
+  }
 
   render() {
     return (
       <div>
-        <h1>Jobs</h1>
+        <h1></h1>
+        <Button keyword={this.getKeyword} />
         <SimpleExpansionPanel posts = {this.state.posts} classes={{paper:"paper"}} />
-
         {this.state.loading ? this.renderLoading() : this.renderPosts()}
       </div>
     );
+  }
+}
+
+
+class Button extends React.Component {
+  
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.props.keyword}>
+          <input type="text" name="keyword" placeholder="category" />
+          <button>Filter</button>
+        </form>
+      </div>
+    )
   }
 }
 
@@ -108,7 +145,7 @@ class CodingCareers extends React.Component {
   render(){
     return(
       <div>
-        <Jobs />
+        <Jobs language = {'ruby'} />
       </div>
     )
   }
