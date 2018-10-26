91988ca3a169b341e2062dba72071cfc01760e13
Antonija Simic <antonija1023@gmail.com>
2018-10-26 10:59:55 +0200

    prevent submission of empty field

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,14 +3,40 @@ import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Filter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      keyword: ""
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(evt) {
+    this.setState({ keyword: evt.target.value });
+    console.log(this.state.keyword)
+  }
+
   render(){
+    const { keyword } = this.state;
+    const isEnabled = keyword.length > 0
     return(
       <header className="header">
         <div className="wrapper">
           <h1 className=""> Show me the reddits! </h1>
             <form onSubmit={this.props.keyword}>
-              <input type="text" name="keyword" placeholder="Subreddit" className="input-search" />
-              <button type="submit" className="btn-search">
+              <input 
+                type="text" 
+                name="keyword" 
+                placeholder="Subreddit" 
+                className="input-search"
+                value={this.state.keyword}
+                onChange={this.handleInputChange}
+               />
+              <button 
+              type="submit" 
+              disabled={!isEnabled}
+              className="btn-search"
+              >
                 <FontAwesomeIcon icon="search" />
               </button>
             </form>
