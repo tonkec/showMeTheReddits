e4663fd9e37bf480df3063701c023d61195240c3
Antonija Simic <antonija1023@gmail.com>
2018-10-29 18:25:15 +0100

    new beginnings with create-react-app; add express server; create build for production

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import classnames from 'classnames';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import red from '@material-ui/core/colors/red';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Grid from '@material-ui/core/Grid';
-
 
 const styles = theme => ({
   card: {
@@ -21,15 +17,12 @@ const styles = theme => ({
   },
   actions: {
     display: 'flex',
-  },
-  masonry: {
-    marginBottom: "20px"
   }
 });
 class RedditCard extends React.Component {
 
   render() {
-    const { classes, posts } = this.props;
+    const { classes } = this.props;
     return(
       <Grid container spacing={24}>
         {this.props.posts.map((post, i) =>
--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Filter extends React.Component {
